ffe05386827f41b70e0d8f0a52c09d41e3580667
NandawulaViola <violettah3115@gmail.com>
2022-08-14 12:11:36 -0700

    added project collections

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -1,5 +1,17 @@
 const logins = require('./logins/logins.service.js');
+const employees = require('./employees/employees.service.js');
+const customers = require('./customers/customers.service.js');
+const accounts = require('./accounts/accounts.service.js');
+const reservations = require('./reservations/reservations.service.js');
+const shifts = require('./shifts/shifts.service.js');
+const salonServices = require('./salon-services/salon-services.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(logins);
+  app.configure(employees);
+  app.configure(customers);
+  app.configure(accounts);
+  app.configure(reservations);
+  app.configure(shifts);
+  app.configure(salonServices);
 };
---
+++ server/src/services/reservations/reservations.class.js
@@ -0,0 +1,5 @@
+const { Service } = require('feathers-mongoose');
+
+exports.Reservations = class Reservations extends Service {
+  
+};
